44941bac0a603ee6ae40360a6a1e75d6e087dbe7
Doray Gisele Owens <doray.owens@gmail.com>
2016-09-19 16:59:54 -0400

    cards completed

---
+++ script.js
@@ -0,0 +1,9 @@
+//make each card clickable
+
+//card turns over in both directions when clicked.
+
+//user can keep track of right and wrong cards somehow.
+
+//any flashcards marked correct should disappear.
+
+//incorrect flashcards should reappear until they are right.
