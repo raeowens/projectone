5cf564e7444db90a941f7d5dec84b3eb9da68f1c
Doray Gisele Owens <doray.owens@gmail.com>
2016-09-20 22:47:57 -0400

    all states included, colors and font changed.

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ var card = $(".card");
 var currentCard = 0;
 var button = $(".button")
 
+
 card.hide()
 card.eq(currentCard).show()
-
 
 
 // Function to display answers
@@ -31,7 +31,16 @@ function nextCard(){
 card.on("click", showAnswer);
 button.on("click", nextCard);
 
-//card turns over in both directions when clicked.
+//if last card is clicked
+
+
+//tell user they are at the end
+
+/*
+$('button').on('click', showAlert(){
+    $alert('E N D')
+});
+*/
 
 //user can keep track of right and wrong cards somehow.
 
